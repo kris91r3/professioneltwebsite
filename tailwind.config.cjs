c00c8cbbbd7669a92a81e58feef1429c4ec82980
Kristina Andersen <kris91r3@stud.kea.dk>
2022-09-20 16:29:05 +0200

    nr 1

---
+++ tailwind.config.cjs
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,vue,js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      white: "#ffffff",
+      offwhite: "#eeeeee",
+      black: "#000000",
+      grey: "#606060",
+      Lime: "#ccff00",
+      Limehover: "#96bb00",
+      Darkgrey: "#4b4b4b",
+
+      backgroundSize: {
+        auto: "auto",
+        cover: "cover",
+        contain: "contain",
+        50: "50%",
+        16: "4rem",
+      },
+    },
+    extend: {
+      screens: {
+        sm: "480px",
+        // => @media (min-width: 480px) { ... }
+
+        md: "710px",
+        // => @media (min-width: 710px) { ... }
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
+};
