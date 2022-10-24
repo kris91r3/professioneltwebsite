83d2de3fbc2836ef7e30a3235a746498113408c4
Kristina Andersen <kris91r3@stud.kea.dk>
2022-10-24 13:07:48 +0200

    animation

--- src/main.js
+++ src/main.js
@@ -1,3 +1,28 @@
 import "./style.css";
 /* Import af Motion One bibliotek */
 import { animate, stagger, inView, scroll, timeline } from "motion";
+
+// skoleprojekt animation
+inView("#spil", ({ target }) => {
+  animate(
+    target.querySelectorAll("div"),
+    { x: [-2000, 0] },
+    { duration: 1.5, delay: stagger(0.9, { start: 0.2 }) }
+  );
+
+  return () => {
+    animate(target, { x: [-2000, 0] }, { duration: 1 });
+  };
+});
+
+inView("#webfeature", ({ target }) => {
+  animate(
+    target.querySelectorAll("div"),
+    { x: [-2000, 0] },
+    { duration: 1.5, delay: stagger(0.9, { start: 0.2 }) }
+  );
+
+  return () => {
+    animate(target, { x: [-2000, 0] }, { duration: 1 });
+  };
+});
