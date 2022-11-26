19e1c3835ad45976f05334d94e2c2c28d1b6e33c
Kristina Andersen <kris91r3@stud.kea.dk>
2022-11-26 09:35:50 +0100

    et projekt mere

--- src/main.js
+++ src/main.js
@@ -3,23 +3,11 @@ import "./style.css";
 import { animate, stagger, inView, scroll, timeline } from "motion";
 
 // skoleprojekt animation
-inView("#spil", ({ target }) => {
+inView("#skoleprojekter", ({ target }) => {
   animate(
     target.querySelectorAll("div"),
     { x: [-2000, 0] },
-    { duration: 0.7, delay: stagger(1, { start: 0.2 }) }
-  );
-
-  return () => {
-    animate(target, { x: [-2000, 0] }, { duration: 0.7 });
-  };
-});
-
-inView("#webfeature", ({ target }) => {
-  animate(
-    target.querySelectorAll("div"),
-    { x: [-2000, 0] },
-    { duration: 0.7, delay: stagger(1, { start: 0.2 }) }
+    { duration: 0.7, delay: stagger(0.5, { start: 0.1 }) }
   );
 
   return () => {
